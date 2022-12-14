fa4b162d2058d9f8518e5464b584a6618c2dc917
Simon Y <therealkjai139@gmail.com>
2022-12-14 02:38:40 -0500

    skele

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className='top-nav'>
+        <h3>Logo</h3>
+        <ul className='nav-list'>
+          <li>Home</li>
+          <li>Shop</li>
+        </ul>
+      </nav>
     </div>
   );
 }
