47c8f2ced2695b905112f2ef7d6b3f9fbe11d7cb
Simon Y <therealkjai139@gmail.com>
2022-12-15 19:09:27 -0500

    fontpage layout

--- src/App.js
+++ src/App.js
@@ -5,16 +5,36 @@ import { Home } from './Components/Home';
 import { Shop } from './Components/Shop';
 import { ErrorPage } from './Components/ErrorPage';
 import { NavBar } from './Components/NavBar';
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [events, setEvents] = useState([])
+
+  useEffect( () => {
+      const fetchData = async () => {
+          const data = await fetch('https://valorant-api.com/v1/bundles')
+          const json = await data.json()
+
+          console.log(json)
+          setEvents(json.data)
+          
+
+      }
+
+      fetchData().catch(console.error)
+      
+      
+  }, [])
+
   return (
     <div className="App">
       
       <BrowserRouter>
       <NavBar />
       <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/home' exact element={<Home />} />
+        <Route path='/' element={<Home events={events}/>} />
+        <Route path='/home' exact element={<Home events={events} />} />
         <Route path ='/shop' element={<Shop />} />
         <Route path='*' element={<ErrorPage />} />
       </Routes>
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,39 +2,45 @@ import React, { useEffect, useState } from "react";
 
 
 
-const Home = () => {
+const Home = ({events}) => {
 
-    const [events, setEvents] = useState([])
-
-    useEffect( () => {
-        const fetchData = async () => {
-            const data = await (await fetch('https://valorant-api.com/v1/bundles')).json()
-
-            console.log(data)
-            setEvents(data.data)
-
-        }
-
-        fetchData().catch(console.error)
-    }, [])
+    const [showItemsNum, setShowItemsNum] = useState(9)
 
     const renderEvents = () => {
-        return events.map((value, key) => 
-            <div key={value.uuid} className="eventCards" style={{
-                backgroundImage: `url(${value.displayIcon})`
-            }}>
+        return events.slice(0, showItemsNum).map((value, key) => 
 
-            </div>
+        <div key={events[0] ? value.uuid : ""} style={{
+            backgroundImage: `${events[0] ? `url(${value.displayIcon})` : ''}`
+        }} className="eventCards">
+            <p className="cardTxt">{events[0] ? value.description : "Loading..."}</p>
+            
+        </div>
         
 
         )
             
     }
 
+
+    const filler = () => {
+        <div key={events[0] ? events[0].uuid : ""} style={{
+            backgroundImage: `${events[0] ? `url(${events[0].displayIcon})` : ''}`
+        }} className="eventCards">
+            <p>${events[0] ? events[0].description : "Loading..."}</p>
+            
+        </div>
+    }
+
+    
+
     return (
         <div className="eventGrid">
             {renderEvents()}
+            
+
         </div>
+            
+        
     )
 }
 
